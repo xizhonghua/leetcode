21f401d1ee12fe103ef30adfe22a4ff5875ecb00
xizhonghua <xizhonghua@gmail.com>
2013-12-31 23:28:53 -0500

    update PathSum

--- leetcode/PathSum.cpp
+++ leetcode/PathSum.cpp
@@ -10,23 +10,13 @@
 class Solution {
 public:
     bool hasPathSum(TreeNode *root, int sum) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if(root == NULL) return false;        
-        if(root->left == NULL && root->right == NULL)
-        {
-            return sum == root->val;
-        }
+        if(!root) return false;
+        int t = sum - root->val;
+        if(!root->left && !root->right && t == 0) return true;
         
-        bool l = false, r = false;
-        if(root->left)
-        {            
-            l = hasPathSum(root->left, sum - root->val);
-        }
-        if(root->right)
-        {
-            r = hasPathSum(root->right, sum - root->val);
-        }
-        return l || r;
+        if(hasPathSum(root->left, t)) return true;
+        if(hasPathSum(root->right, t)) return true;
+        
+        return false;
     }
 };
