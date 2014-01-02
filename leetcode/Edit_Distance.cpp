7274a3f700434544ebc12c459d193dd175c5bd28
xizhonghua <xizhonghua@gmail.com>
2014-01-02 17:01:41 -0500

    update

--- leetcode/Edit_Distance.cpp
+++ leetcode/Edit_Distance.cpp
@@ -1,43 +1,28 @@
 class Solution {
 public:
-class Solution {
-public:
     int minDistance(string word1, string word2) {
-        int l1 = word1.length();
-        int l2 = word2.length();       
-        vector<vector<int> > f;        
-        for(int i=0;i<=l1;i++)
-        {
-            f.push_back(vector<int>());
-            f[i].reserve(l2+1);
-            for(int j=0;j<=l2;j++)
-            {
-                f[0][j] = j;
-                f[i][0] = i;
-            }
-        }
+        int l1 = word1.length(), l2 = word2.length();
+        if(!l1 || !l2) return l1 | l2;
+        
+        vector<vector<int> > f;
+        for(int i=0;i<=l1;i++) f.push_back(vector<int>(l2+1));
+        
+        f[0][0] = 0;
+        for(int i=1;i<=l1;i++) f[i][0] = i;
+        for(int j=1;j<=l2;j++) f[0][j] = j;
+        
         for(int i=1;i<=l1;i++)
-            for(int j=1;j<=l2;j++)
+            for(int j=1;j<=l2;j++) 
             {
-                int d = 1;
-                if(word1[i-1] == word2[j-1]) 
-                    d = 0;
+                f[i][j] = INT_MAX;
+                if(word1[i-1] == word2[j-1])
+                    f[i][j] = f[i-1][j-1];
                 
-                f[i][j] = min(min(f[i][j-1], f[i-1][j]) + 1, f[i-1][j-1]+d);
-                
+                f[i][j] = min(f[i][j], f[i-1][j-1]+1);
+                f[i][j] = min(f[i][j], f[i][j-1]+1);
+                f[i][j] = min(f[i][j], f[i-1][j]+1);
             }
-        
-        return f[l1][l2];       
-    }
-};
-    
-    vector<vector<string>> partition(string s) {        
-        vector<vector<string> > ans;
-        vector<string> cur;
-        
-        p(s, 0, ans, cur);            
-        
-        return ans;
-        
+            
+        return f[l1][l2];
     }
 };
