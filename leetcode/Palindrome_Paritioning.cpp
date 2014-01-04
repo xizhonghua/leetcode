2bf80217319562d458cf253c6c6d299bc559579c
xizhonghua <xizhonghua@gmail.com>
2014-01-03 20:46:52 -0500

    update

--- leetcode/Palindrome_Paritioning.cpp
+++ leetcode/Palindrome_Paritioning.cpp
@@ -1,47 +1,28 @@
 class Solution {
 public:
-    bool t(string s)
-    {
-        int l = s.length();
-        
-        for(int i=0;i<l/2;i++)
-        {
-            if(s[i] != s[l-i-1]) return false;
-        }
-        
-        return true;
+    vector<vector<string>> partition(string s) {
+        vector<vector<string> > ans;
+        vector<string> cur;
+        dfs(s, ans, cur);
+        return ans;
     }
     
-    void p(string& s, int a, vector<vector<string>>& ans, vector<string>& cur)
-    {        
+private:
+    void dfs(string s, vector<vector<string> >& ans,  vector<string>& cur) {
         int l = s.length();
-        
-        if(a == l)
-        {
+        if(l == 0) {
             ans.push_back(cur);
             return;
         }
-        
-        for(int i=a;i<l;i++)
-        {
-            string ss = s.substr(a, i-a+1);
-            if(t(ss))
-            {
-                cur.push_back(ss);
-                p(s, i+1, ans, cur);
-                cur.pop_back();
-            }
-            
-        }
-    }
-    
-    vector<vector<string>> partition(string s) {        
-        vector<vector<string> > ans;
-        vector<string> cur;
-        
-        p(s, 0, ans, cur);            
-        
-        return ans;
-        
+        for(int i=1;i<=l;i++) {
+            bool flag = true;
+            for(int j=0;j<i/2 && flag;j++)
+                if(s[j] != s[i-j-1]) flag = false;
+            if(!flag) continue;
+            string ss = s.substr(0, i);
+            cur.push_back(ss);
+            dfs(s.substr(i), ans, cur);
+            cur.pop_back();
+        }    
     }
 };
