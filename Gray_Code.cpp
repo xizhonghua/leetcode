9127e49ab219a57134a822756615ca79a73412d9
Zhonghua Xi <xizhonghua@gmail.com>
2015-01-05 02:25:40 -0500

    Update Gray_Code.cpp

--- Gray_Code.cpp
+++ Gray_Code.cpp
@@ -6,9 +6,8 @@ public:
         
         auto l = grayCode(n-1);
         auto r = l;
-        std::reverse(r.begin(), r.end());
         for(auto& e : r) e |= (1<<(n-1));
-        l.insert(l.end(), r.begin(), r.end());
+        l.insert(l.end(), r.rbegin(), r.rend());
         
         return l;
     }
