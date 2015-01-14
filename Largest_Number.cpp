64eb449067c52dde75913d6ff75187103663b91c
Zhonghua Xi <xizhonghua@gmail.com>
2015-01-13 22:49:31 -0500

    string version

--- Largest_Number.cpp
+++ Largest_Number.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     string largestNumber(vector<int> &num) {
-        map<int, long long> m;
+        map<int, string> m;
         for(auto n: num)
         {
-            if(n==0) {
-                m[n] = 0;
-                continue;
-            }
+            stringstream ss;
+            ss<<n;
             
-            long long t = n;
-            long long first = t;
-            long long last = t % 10;
-            while(first>=10) first/=10;
-            int padding = last < first ? first : last;
+            m[n] = ss.str();
+            string& s = m[n];
             
-            while(t<1e12)
-                t = t*10 + padding;
-            m[n] = t;
+            char padding = s.back() < s.front() ? s.front() : s.back();
+            
+            while(s.length()<12)
+                s+= padding;
         }
         
-        sort(num.begin(), num.end(), [&m](const int &a, const int &b){
-           return m[a] > m[b];
-        });
+        sort(num.begin(), num.end(), [&m](const int &a, const int &b) { return m[a] > m[b]; });
         
         stringstream ss;
-        for(auto n : num) {
-            ss<<n;
-        }
+        for(auto n : num) ss<<n;
         
         string s = ss.str();
         
