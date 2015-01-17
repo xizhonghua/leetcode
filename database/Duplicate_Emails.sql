3a61d9a59fe4a53376ef9930c11557552b0baffe
Zhonghua Xi <xizhonghua@gmail.com>
2015-01-17 10:51:15 -0500

    Update Duplicate_Emails.sql

--- database/Duplicate_Emails.sql
+++ database/Duplicate_Emails.sql
@@ -1,20 +1,20 @@
-Write a SQL query to find all duplicate emails in a table named Person.
+-- Write a SQL query to find all duplicate emails in a table named Person.
 
-+----+---------+
-| Id | Email   |
-+----+---------+
-| 1  | a@b.com |
-| 2  | c@d.com |
-| 3  | a@b.com |
-+----+---------+
-For example, your query should return the following for the above table:
+-- +----+---------+
+-- | Id | Email   |
+-- +----+---------+
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+-- +----+---------+
+-- For example, your query should return the following for the above table:
 
-+---------+
-| Email   |
-+---------+
-| a@b.com |
-+---------+
-Note: All emails are in lowercase.
+-- +---------+
+-- | Email   |
+-- +---------+
+-- | a@b.com |
+-- +---------+
+-- Note: All emails are in lowercase.
 
 SELECT DISTINCT Email 
 FROM Person p1
