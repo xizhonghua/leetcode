bdfc8afb6cfb44bc58365c03d9b612072f473ed5
Zhonghua Xi <xizhonghua@gmail.com>
2015-03-15 09:50:07 -0400

    Create Number_of_1_Bits.cpp

---
+++ Number_of_1_Bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int ans = 0;
+        while(n) {
+            if(n%2) ans++;
+            n>>=1;
+        }
+        return ans;
+    }
+};
