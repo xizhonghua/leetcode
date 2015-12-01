3805b0d643f91b06f67a5d974c80deb465d91fc5
Zhonghua Xi <xizhonghua@gmail.com>
2015-12-01 02:23:58 -0500

    Update House-Robber.cpp

--- algorithm/House-Robber.cpp
+++ algorithm/House-Robber.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         if(nums.size() == 0) return 0;
-        if(nums.size() == 1) return nums[0];
-        if(nums.size() == 2) return max(nums[0], nums[1]);
         
-        int f1 = max(nums[0],nums[1]);
-        int f2 = nums[0];
-        int current = -1;
-        for(int i=2;i<nums.size();++i) {
-            current = max(f2 + nums[i], f1);
+        int f1 = nums[0];
+        int f2 = 0;
+        int opt = nums[0];
+        for(int i=1;i<nums.size();++i) {
+            opt = max(f2 + nums[i], f1);
             f2 = f1;
-            f1 = current;
+            f1 = opt;
         }
         
-        return current;
+        return opt;
     }
 };
